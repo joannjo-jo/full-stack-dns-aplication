11ed86579fdcac45e4a7d94a87aa35fdee633e62
Jo Ann J <joannjo1729@gmail.com>
2025-07-11 15:38:08 +0530

    Initial commit with MongoDB URI support

--- backend/DBconnection.js
+++ backend/DBconnection.js
@@ -1,6 +1,7 @@
 var mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/shop");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/shop");
+
 
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error"));
