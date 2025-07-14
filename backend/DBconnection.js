615fb0894ab9b896ad642818da8aebe2f346160f
Jo Ann J <joannjo1729@gmail.com>
2025-07-14 10:20:08 +0530

    udate: mongodb connection

--- backend/DBconnection.js
+++ backend/DBconnection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://127.0.0.1:27017/shop");
+mongoose.connect("mongodb+srv://joannjo1729:7RXb7h09Nr5MZFfl@cluster0.i9aunkp.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0");
 
 const db = mongoose.connection;
 
