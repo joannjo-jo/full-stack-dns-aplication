e43f633afc1e120fdeaa92322d4e0f34b9258eb9
Jo Ann J <joannjo1729@gmail.com>
2025-07-14 11:49:07 +0530

    udate connection

--- backend/DBconnection.js
+++ backend/DBconnection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://joannjo1729:kUCv1pdLIwIq0pNC@cluster0.vr4iekl.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0")
+mongoose.connect("http://127.0.0.1:8004")
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
